b27d0b4a85c13ed8d46641b3be32e47ef8cbb4b0
Thomas Geymayer <tomgey@gmail.com>
2012-05-02 12:32:53 +0200

    Fix rendering with desktop blend

--- components/renderer_gl/blurY.glsl
+++ components/renderer_gl/blurY.glsl
@@ -1,5 +1,6 @@
 uniform sampler2D inputTex;
 uniform float scale;
+uniform int normalize_color;
 
 const int size = 4;
 //float weights[3] = float[](8/16.0, 3.5/16.0, 0.5/16.0);
@@ -13,7 +14,7 @@ void main()
   for(int i = -size + 1; i < size; ++i)
     color += weights[abs(i)] * texture2D(inputTex, gl_TexCoord[0].xy + vec2(0,i) * dims_inv);
 
-  if( color.a > 0.0001 )
+  if( normalize_color && color.a > 0.0001 )
     color.rgb /= color.a;
 
   gl_FragColor = color;
